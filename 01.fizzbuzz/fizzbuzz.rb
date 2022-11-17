b940082b9189b6cea96e5a2f4bb17ce0876f5ace
jiro <seijiro.watanabe@gmail.com>
2022-11-17 17:06:22 +0900

    Add fizzbuzz

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,14 +1,13 @@
-for x in 1..20
-	if x% (3 * 5)==0
-		puts "FizzBuzz"
-	elsif x% 3==0
-		puts "Fizz"
-	elsif x% 5==0
-		puts "Buzz"
-	else
-		puts x
-	end
-end
+(1..20).each do  |x|
 
-			
-		
+  if x % (3 * 5) == 0
+		puts "FizzBuzz"
+  elsif x % 3 == 0
+		puts "Fizz"
+  elsif x % 5 == 0
+		puts "Buzz"
+  else
+		puts x
+  end
+
+end
