0a3e1e8f9e48f63775ff148e6bd605f2f9885e59
jiro <seijiro.watanabe@gmail.com>
2022-11-17 18:52:31 +0900

    Modify スペース修正

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,9 +1,9 @@
 (1..20).each do |x|
-  if x %　(3 * 5)== 0
+  if x %　(3 * 5)　== 0
     puts "FizzBuzz"
   elsif x %　3 == 0
     puts "Fizz"
-  elsif x %　5== 0
+  elsif x %　5　== 0
     puts "Buzz"
   else
     puts x
